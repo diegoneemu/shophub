4223ab1fe09cf3626d7757c0bc5bbf6d6dd3020c
Diego Augusto <diego.augusto@neemu.com>
2021-07-25 17:09:41 -0300

    feat(<Logo />): Should be render a store name

---
+++ src/components/Logo/index.tsx
@@ -0,0 +1,8 @@
+import React, { FunctionComponent } from "react";
+
+export const Logo: FunctionComponent<{}> = () => (
+  <div>
+    <img src="asdf" alt="shophub logo" />
+    shophub
+  </div>
+);
