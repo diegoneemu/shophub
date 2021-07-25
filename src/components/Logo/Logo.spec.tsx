576d79006d3a978fc21c26c1c911f3ee05fc19f7
Diego Augusto <diego.augusto@neemu.com>
2021-07-25 17:11:01 -0300

    test(<Logo />): Ensure that a logo image is render

--- src/components/Logo/Logo.spec.tsx
+++ src/components/Logo/Logo.spec.tsx
@@ -8,4 +8,11 @@ describe("<Logo />", ()=>{
     const logo = screen.getByText(/shophub/i);
     expect(logo).toBeInTheDocument();
   });
+
+  test('Should be render a logo image', () => {
+    render(<Logo />);
+    const logo = screen.getByAltText(/shophub logo/i);
+    const src = logo.getAttribute("src");
+    expect(src).toEqual('img/logotipo.png');
+  })
 })
