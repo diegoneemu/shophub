83999a978a970f849cbe09fe4980763ed059266e
Diego Augusto <diego.augusto@neemu.com>
2021-07-25 21:27:36 -0300

    test(<CartSummary />): create test file and spec todos

---
+++ src/components/CartSummary/CartSummary.spec.tsx
@@ -0,0 +1,7 @@
+import { render } from '@testing-library/react';
+
+describe("<CartSummary />", () => {
+  test.todo("Should be render a cart summary with correct items quantity");
+  test.todo("Should be render a cart summary with correct items subtotal");
+  test.todo("Should be render a cart summary with correct bag icon");
+})
