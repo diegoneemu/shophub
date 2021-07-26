ff7ba6f32db46c0c5d041ef149153c65f54202b6
Diego Augusto <diego.augusto@neemu.com>
2021-07-25 21:36:16 -0300

    test(<CartSummary />): ensure that render a cart summary with correct items quantity

--- src/components/CartSummary/CartSummary.spec.tsx
+++ src/components/CartSummary/CartSummary.spec.tsx
@@ -1,7 +1,16 @@
-import { render } from '@testing-library/react';
+import { render, screen } from '@testing-library/react';
+
+const CartSummary = () => (<div></div>)
 
 describe("<CartSummary />", () => {
-  test.todo("Should be render a cart summary with correct items quantity");
+  test("Should be render a cart summary with correct items quantity", ()=>{
+    render(<CartSummary />);
+
+    const tooltip = screen.queryByRole("tooltip");
+
+    expect(tooltip).toHaveTextContent("3");
+  });
+  
   test.todo("Should be render a cart summary with correct items subtotal");
   test.todo("Should be render a cart summary with correct bag icon");
 })
