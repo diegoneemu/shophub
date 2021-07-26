95b18407df389db359c86802d58209fd184441a9
Diego Augusto <diego.augusto@neemu.com>
2021-07-25 23:08:23 -0300

    refactor(<CartSummary />): remove unnecessary render debug

--- src/components/CartSummary/CartSummary.spec.tsx
+++ src/components/CartSummary/CartSummary.spec.tsx
@@ -9,7 +9,7 @@ describe("<CartSummary />", () => {
   });
 
   test("Should be render a cart summary with correct items subtotal", () => {
-    const {debug} = render(<CartSummary quantity={3} subtotal={12345} />)
+    render(<CartSummary quantity={3} subtotal={12345} />)
     const subtotal = screen.queryByText(/^R\$\s(?!0+\,00)((\d{1,3}\.)*)(\d{1,3}),(\d{2})$/);
     expect(subtotal).toHaveTextContent("R$ 12.345,00");
   });
