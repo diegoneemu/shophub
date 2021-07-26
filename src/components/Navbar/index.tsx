7649ef450ff443bf5e4fdd366ec4e5f61cbba6e8
Diego Augusto <diego.augusto@neemu.com>
2021-07-25 21:13:25 -0300

    feat(<Navbar />): Ensure that render nav bar component with correct items

--- src/components/Navbar/index.tsx
+++ src/components/Navbar/index.tsx
@@ -1,3 +1,9 @@
 import { FunctionComponent } from "react";
+import menu from "../../config/menu";
+import { NavbarItems } from "./NavbarItems";
 
-export const Navbar: FunctionComponent = () => (<nav></nav>);
+export const Navbar: FunctionComponent = () => (
+  <nav>
+    <NavbarItems items={menu} />
+  </nav>
+);
