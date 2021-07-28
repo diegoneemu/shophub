2e9324cb7c8f66252cb83ff4b79a206008d643ac
Diego Augusto <diego.augusto@neemu.com>
2021-07-28 01:13:56 -0300

    refactor(<ProductCard />): create make sut dependencies function

--- src/components/ProductCard/ProductCard.spec.tsx
+++ src/components/ProductCard/ProductCard.spec.tsx
@@ -1,20 +1,30 @@
-import {render, screen} from "@testing-library/react";
+import { render, screen } from "@testing-library/react";
 import { Product } from "../../types";
 import { ProductCard } from ".";
 
-describe("<ProductCard />", ()=> {
-  test("Should be render a product card with correct name of product", ()=>{
-    const product: Product = {
-      name: 'Popular Shoes Trends'
-    }
+describe("<ProductCard />", () => {
+  test("Should be render a product card with correct name of product", () => {
+    const { product } = makeSutDependencies();
 
     render(<ProductCard product={product} />);
     const productNameElement = screen.queryByText(product.name);
     expect(productNameElement).toHaveTextContent(product.name);
-  })
+  });
 
-  test.todo("Should be render a product card with correct coast of product")
-  test.todo("Should be render a product card with correct image of product")
-  test.todo("Should be render a product card with bag icon")
-  test.todo("Should be render a product card with details button")
-})
+  test.todo("Should be render a product card with correct coast of product");
+  test.todo("Should be render a product card with correct image of product");
+  test.todo("Should be render a product card with bag icon");
+  test.todo("Should be render a product card with details button");
+});
+
+type SutDependenciesParams = {
+  product: Product;
+};
+
+function makeSutDependencies(): SutDependenciesParams {
+  const product: Product = {
+    name: "Popular Shoes Trends",
+  };
+
+  return { product };
+}
