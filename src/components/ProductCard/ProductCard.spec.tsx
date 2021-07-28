9504c73b160bacf7243881066264dbe7db2671e3
Diego Augusto <diego.augusto@neemu.com>
2021-07-28 00:13:21 -0300

    test(<ProductCard />): create test todos

---
+++ src/components/ProductCard/ProductCard.spec.tsx
@@ -0,0 +1,9 @@
+import {render} from "@testing-library/react";
+
+describe("<ProductCard />", ()=> {
+  test.todo("Should be render a product card with correct name of product")
+  test.todo("Should be render a product card with correct coast of product")
+  test.todo("Should be render a product card with correct image of product")
+  test.todo("Should be render a product card with bag icon")
+  test.todo("Should be render a product card with details button")
+})
