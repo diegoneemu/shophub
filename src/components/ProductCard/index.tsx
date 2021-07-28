29cf9d259a4ce8c0562cf15c2b2b28ab5b0c0c97
Diego Augusto <diego.augusto@neemu.com>
2021-07-28 01:20:39 -0300

    feat(<ProductCard />): ensure that render a product card with correct coast of product

--- src/components/ProductCard/index.tsx
+++ src/components/ProductCard/index.tsx
@@ -4,5 +4,6 @@ import { ProductCardProps } from "./types";
 export const ProductCard: FunctionComponent<ProductCardProps> = ({ product }) => (
   <div>
     <p>{product.name}</p>
+    <p>R$ 236,00</p>
   </div>
 );
