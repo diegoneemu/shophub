b68f9ddee0c09e3ea9698bb138218ef455018c72
Diego Augusto <diego.augusto@neemu.com>
2021-07-28 01:25:47 -0300

    refactor(<ProductCard />): ensure that render a product card with correct coast of product

--- src/components/ProductCard/index.tsx
+++ src/components/ProductCard/index.tsx
@@ -1,9 +1,22 @@
-import { FunctionComponent } from "react";
+import { FunctionComponent, useMemo } from "react";
 import { ProductCardProps } from "./types";
 
-export const ProductCard: FunctionComponent<ProductCardProps> = ({ product }) => (
-  <div>
-    <p>{product.name}</p>
-    <p>R$ 236,00</p>
-  </div>
-);
+export const ProductCard: FunctionComponent<ProductCardProps> = ({
+  product,
+}) => {
+  const coastFormatted = useMemo(
+    () =>
+      `R$ ${product.coast.toLocaleString("pt-BR", {
+        maximumFractionDigits: 2,
+        minimumFractionDigits: 2,
+      })}`,
+    [product]
+  );
+
+  return (
+    <div>
+      <p>{product.name}</p>
+      <p>{coastFormatted}</p>
+    </div>
+  );
+};
