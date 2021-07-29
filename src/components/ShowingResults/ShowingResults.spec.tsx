b46ded06a9de6675a061ce5370ede7d3014add46
Diego Augusto <diego.augusto@neemu.com>
2021-07-29 01:47:28 -0300

    test(<ShowingResult />): ensure that render a correct interval that view in list

--- src/components/ShowingResults/ShowingResults.spec.tsx
+++ src/components/ShowingResults/ShowingResults.spec.tsx
@@ -1,4 +1,16 @@
+import { render, screen } from "@testing-library/react";
+import { FunctionComponent } from "react";
+
+const ShowingResults: FunctionComponent<{currentPage: number, totalResults: number}> = () => (
+  <p></p>
+)
+
 describe("<ShowingResults />", () => {
-  test.todo("Should be render a correct interval that view in list");
+  test("Should be render a correct interval that view in list", () => {
+    render(<ShowingResults currentPage={1} totalResults={36} />)
+    const showingResultComponent = screen.queryByText(/Showing \d+-\d+ of \d+ results/);
+    expect(showingResultComponent).toHaveTextContent("Showing 1-12 of 36 results");
+  });
+
   test.todo("Should be render a correct totals of itens in the list");
 })
