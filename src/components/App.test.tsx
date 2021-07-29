fab0278274bfca56d4ee549cf74c4a9c8208c657
Diego Augusto <diego.augusto@neemu.com>
2021-07-29 07:31:37 -0300

    refactor(<App />): use menu config to ensure that render a correct navbar

--- src/components/App.test.tsx
+++ src/components/App.test.tsx
@@ -1,28 +1,21 @@
-import React from 'react';
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import { render, screen } from "@testing-library/react";
+import App from "./App";
+import menuList from "../config/menu";
+import { MenuItem } from "../types/MenuItem";
 
-describe('<App />', () => {
+describe("<App />", () => {
   test("Ensure that render logo", () => {
     render(<App />);
     const shopname = screen.queryByText(/shophub/i);
     expect(shopname).toBeInTheDocument();
-  })
+  });
 
   test("Should be render a navbar", () => {
-    const navbarItems = ['Home', 'Shop', 'Blog', 'Contact'];
     render(<App />);
 
-    const home = screen.queryByText(navbarItems[0]);
-    expect(home).toBeInTheDocument();
-
-    const shop = screen.queryByText(navbarItems[1]);
-    expect(shop).toBeInTheDocument();
-
-    const blog = screen.queryByText(navbarItems[2]);
-    expect(blog).toBeInTheDocument();
-
-    const contact = screen.queryByText(navbarItems[3]);
-    expect(contact).toBeInTheDocument();
-  })
+    menuList.forEach((menuItem: MenuItem) => {
+      const item = screen.queryByText(menuItem.name);
+      expect(item).toBeInTheDocument();
+    });
+  });
 });
