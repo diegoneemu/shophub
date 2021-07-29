cdbeace8078cd29fc82622016235d46e9d33d7ac
Diego Augusto <diego.augusto@neemu.com>
2021-07-29 02:06:03 -0300

    refactor(<ShowingResult />): reformat code

--- src/components/ShowingResults/ShowingResults.spec.tsx
+++ src/components/ShowingResults/ShowingResults.spec.tsx
@@ -3,26 +3,42 @@ import { ShowingResults } from ".";
 
 describe("<ShowingResults />", () => {
   test("Should be render a correct interval that view in list and total results", () => {
-    render(<ShowingResults currentPage={1} totalResults={36} perPage={12}/>)
-    const showingResultComponent = screen.queryByText(/Showing \d+-\d+ of \d+ results/);
-    expect(showingResultComponent).toHaveTextContent("Showing 1-12 of 36 results");
+    render(<ShowingResults currentPage={1} totalResults={36} perPage={12} />);
+    const showingResultComponent = screen.queryByText(
+      /Showing \d+-\d+ of \d+ results/
+    );
+    expect(showingResultComponent).toHaveTextContent(
+      "Showing 1-12 of 36 results"
+    );
   });
 
   test("Should be render a correct interval in second page", () => {
-    render(<ShowingResults currentPage={2} totalResults={36} perPage={12}/>)
-    const showingResultComponent = screen.queryByText(/Showing \d+-\d+ of \d+ results/);
-    expect(showingResultComponent).toHaveTextContent("Showing 13-24 of 36 results");
+    render(<ShowingResults currentPage={2} totalResults={36} perPage={12} />);
+    const showingResultComponent = screen.queryByText(
+      /Showing \d+-\d+ of \d+ results/
+    );
+    expect(showingResultComponent).toHaveTextContent(
+      "Showing 13-24 of 36 results"
+    );
   });
 
   test("Should be render a correct interval in third page", () => {
-    render(<ShowingResults currentPage={3} totalResults={50} perPage={12}/>)
-    const showingResultComponent = screen.queryByText(/Showing \d+-\d+ of \d+ results/);
-    expect(showingResultComponent).toHaveTextContent("Showing 25-36 of 50 results");
+    render(<ShowingResults currentPage={3} totalResults={50} perPage={12} />);
+    const showingResultComponent = screen.queryByText(
+      /Showing \d+-\d+ of \d+ results/
+    );
+    expect(showingResultComponent).toHaveTextContent(
+      "Showing 25-36 of 50 results"
+    );
   });
 
   test("Should be render a correct interval in last page", () => {
-    render(<ShowingResults currentPage={5} totalResults={50} perPage={12}/>)
-    const showingResultComponent = screen.queryByText(/Showing \d+-\d+ of \d+ results/);
-    expect(showingResultComponent).toHaveTextContent("Showing 49-50 of 50 results");
+    render(<ShowingResults currentPage={5} totalResults={50} perPage={12} />);
+    const showingResultComponent = screen.queryByText(
+      /Showing \d+-\d+ of \d+ results/
+    );
+    expect(showingResultComponent).toHaveTextContent(
+      "Showing 49-50 of 50 results"
+    );
   });
-})
+});
--- src/components/ShowingResults/index.tsx
+++ src/components/ShowingResults/index.tsx
@@ -1,9 +1,17 @@
 import { FunctionComponent } from "react";
 import { ShowingResultsProps } from "./types";
 
-export const ShowingResults: FunctionComponent<ShowingResultsProps> = ({ currentPage, totalResults, perPage }) => {
-
-  const lastOfCurrentPage = (currentPage) * perPage;
+export const ShowingResults: FunctionComponent<ShowingResultsProps> = ({
+  currentPage,
+  totalResults,
+  perPage,
+}) => {
+  const lastOfCurrentPage = currentPage * perPage;
   return (
-  <p>Showing {(currentPage - 1) * perPage + 1}-{lastOfCurrentPage > totalResults ? totalResults : lastOfCurrentPage} of {totalResults} results</p>
-)};
+    <p>
+      Showing {(currentPage - 1) * perPage + 1}-
+      {lastOfCurrentPage > totalResults ? totalResults : lastOfCurrentPage} of{" "}
+      {totalResults} results
+    </p>
+  );
+};
