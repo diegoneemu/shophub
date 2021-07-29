6dca2444e6c131df8dcb5eefc7105986d50a4c3a
Diego Augusto <diego.augusto@neemu.com>
2021-07-29 01:53:04 -0300

    refactor(<ShowingResult />): change tests descriptions and create more tests todos

--- src/components/ShowingResults/ShowingResults.spec.tsx
+++ src/components/ShowingResults/ShowingResults.spec.tsx
@@ -6,11 +6,13 @@ const ShowingResults: FunctionComponent<{currentPage: number, totalResults: numb
 )
 
 describe("<ShowingResults />", () => {
-  test("Should be render a correct interval that view in list", () => {
+  test("Should be render a correct interval that view in list and total results", () => {
     render(<ShowingResults currentPage={1} totalResults={36} />)
     const showingResultComponent = screen.queryByText(/Showing \d+-\d+ of \d+ results/);
     expect(showingResultComponent).toHaveTextContent("Showing 1-12 of 36 results");
   });
 
-  test.todo("Should be render a correct totals of itens in the list");
+  test.todo("Should be render a correct interval in second page");
+  test.todo("Should be render a correct interval in third page");
+  test.todo("Should be render a correct interval in last page");
 })
