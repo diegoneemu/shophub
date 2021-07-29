bcb07dff4e2b3ff46c18a48e70eb027058340835
Diego Augusto <diego.augusto@neemu.com>
2021-07-29 01:39:51 -0300

    test(<ShowingResult />): create a tests todo

---
+++ src/components/ShowingResults/ShowingResults.spec.tsx
@@ -0,0 +1,4 @@
+describe("<ShowingResults />", () => {
+  test.todo("Should be render a correct interval that view in list");
+  test.todo("Should be render a correct totals of itens in the list");
+})
