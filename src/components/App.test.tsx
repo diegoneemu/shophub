e556c03371c15d0e6d5ff741a05d76fadcd8d58e
Diego Augusto <diego.augusto@neemu.com>
2021-07-29 07:02:32 -0300

    test(<App />): ensure that render a correct navbar

--- src/components/App.test.tsx
+++ src/components/App.test.tsx
@@ -8,4 +8,21 @@ describe('<App />', () => {
     const shopname = screen.queryByText(/shophub/i);
     expect(shopname).toBeInTheDocument();
   })
+
+  test("Should be render a navbar", () => {
+    const navbarItems = ['Home', 'Shop', 'Blog', 'Contact'];
+    render(<App />);
+
+    const home = screen.queryByText(navbarItems[0]);
+    expect(home).toBeInTheDocument();
+
+    const shop = screen.queryByText(navbarItems[1]);
+    expect(shop).toBeInTheDocument();
+
+    const blog = screen.queryByText(navbarItems[2]);
+    expect(blog).toBeInTheDocument();
+
+    const contact = screen.queryByText(navbarItems[3]);
+    expect(contact).toBeInTheDocument();
+  })
 });
