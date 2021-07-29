a19893d6791bc9bfbd7ff723b9960f1222439d24
Diego Augusto <diego.augusto@neemu.com>
2021-07-29 06:55:11 -0300

    feat(<App />): ensure that render shopname

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,8 +1,11 @@
 import "./App.css";
+import { Logo } from "./Logo";
 
 function App() {
   return (
-    <div className="App"></div>
+    <div className="App">
+      <Logo />
+    </div>
   );
 }
 
