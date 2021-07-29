5117a48143e432cb7383dcae3b5ae6dd721a2d7f
Diego Augusto <diego.augusto@neemu.com>
2021-07-29 02:04:46 -0300

    feat(<ShowingResult />): ensure that render a correct in last page

--- src/components/ShowingResults/index.tsx
+++ src/components/ShowingResults/index.tsx
@@ -1,6 +1,9 @@
 import { FunctionComponent } from "react";
 import { ShowingResultsProps } from "./types";
 
-export const ShowingResults: FunctionComponent<ShowingResultsProps> = ({ currentPage, totalResults, perPage }) => (
-  <p>Showing {(currentPage - 1) * perPage + 1}-{(currentPage) * perPage} of {totalResults} results</p>
-);
+export const ShowingResults: FunctionComponent<ShowingResultsProps> = ({ currentPage, totalResults, perPage }) => {
+
+  const lastOfCurrentPage = (currentPage) * perPage;
+  return (
+  <p>Showing {(currentPage - 1) * perPage + 1}-{lastOfCurrentPage > totalResults ? totalResults : lastOfCurrentPage} of {totalResults} results</p>
+)};
